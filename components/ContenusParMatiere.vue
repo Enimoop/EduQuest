b8100763c0c988a24a6003a0968829ed954500d2
Enimoop <marine.forcioli@laposte.net>
2024-04-13 10:39:37 +0200

    Add Component ContenusParMatiere

---
+++ components/ContenusParMatiere.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    
+        <h3>Liste des contenus en {{libelle_matiere}}</h3>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">description</th>
+              <th scope="col">date</th>
+              <th scope="col">type</th>
+            </tr>
+          </thead>
+          <tbody v-for="contenu in contenus" :key="contenu.id">
+            <tr>
+              <th scope="row">1</th>
+              <td>{{ contenu.description_contenu }}</td>
+              <td>{{ format(Date(contenu.date_contenu), 'dd/MM/yyyy') }}</td>
+              <td>{{ contenu.type_contenu }}</td>
+            </tr>
+          </tbody>
+        </table>
+
+  </div>
+
+
+
+</template>
+
+<script setup>
+import { format } from 'date-fns';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const contenus = ref([]);
+
+
+onMounted(() => {
+  const route = useRoute()
+  const id = route.params.id;
+  axios.get(`http://localhost:3001/contenus/${id}`)
+    .then(response => {
+      contenus.value = response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching contents:', error);
+    });
+});
+</script>
