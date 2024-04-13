2974f4a2efcaa89fef5d7b3b64c9c2ba80834a99
Enimoop <marine.forcioli@laposte.net>
2024-04-13 10:38:39 +0200

    Add component listeMatiere

--- components/ListeMatiere.vue
+++ components/ListeMatiere.vue
@@ -1,12 +1,17 @@
 <template>
-<h1>Liste des matières</h1>
-    <ul>
-      <li v-for="matiere in matieres" :key="matieres.id_matiere">
-        {{ matiere.id_matiere }} {{ matiere.libelle_matiere }} 
-      </li>
-    </ul>
-
+  <!-- <h3>Listes des matières</h3> -->
+  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
+    <div v-for="matiere in matieres" :key="matiere.id" class="col mb-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{{ matiere.libelle_matiere }}</h5>         
+          <a :href="'/contenu/' +matiere.id" class="card-link">Card link</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
 
 <script setup>
   import { ref, onMounted } from 'vue';
