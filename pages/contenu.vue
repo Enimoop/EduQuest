602f92783b7adf8c016e4febe3f5af7191da3a94
Enimoop <marine.forcioli@laposte.net>
2024-04-13 10:24:13 +0200

    Add pages and routing

---
+++ pages/contenu.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <br>
+    <ListeMatiere :matieres="matieres" />
+    <NuxtPage />
+</div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+import 'bootstrap/dist/css/bootstrap.css'
+import ListeMatiere from '../components/ListeMatiere.vue';
+
+
+const matieres = ref([]);
+
+</script>
---
+++ pages/contenu/[id].vue
@@ -0,0 +1,17 @@
+
+<template>
+    <div>
+    <ContenusParMatiere :contenus_matiere="contenus_matiere" />
+</div>
+</template>
+
+
+<script setup>
+
+import { ref } from 'vue';
+import 'bootstrap/dist/css/bootstrap.css'
+import ContenusParMatiere from '../../components/ContenusParMatiere.vue';
+
+const contenus_matiere = ref([]);
+
+</script>
