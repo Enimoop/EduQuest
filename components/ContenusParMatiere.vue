2ebd5ce8a04f04ddc60089e0138da2c73ebf7c52
Enimoop <marine.forcioli@laposte.net>
2024-04-19 13:59:23 +0200

    Add ContenuParMatiere

--- components/ContenusParMatiere.vue
+++ components/ContenusParMatiere.vue
@@ -1,31 +1,27 @@
 <template>
   <div class="container">
-    
-        <h3>Liste des contenus en {{libelle_matiere}}</h3>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">description</th>
-              <th scope="col">date</th>
-              <th scope="col">type</th>
-            </tr>
-          </thead>
-          <tbody v-for="contenu in contenus" :key="contenu.id">
-            <tr>
-              <th scope="row">1</th>
-              <td>{{ contenu.description_contenu }}</td>
-              <td>{{ format(Date(contenu.date_contenu), 'dd/MM/yyyy') }}</td>
-              <td>{{ contenu.type_contenu }}</td>
-            </tr>
-          </tbody>
-        </table>
-
+    <h3 class="my-4">Liste des contenus en {{ libelle_matiere }}</h3>
+    <div class="row">
+      <div class="col-md-4">
+        <router-link
+        v-for="contenu in contenus"
+        :key="contenu.id"
+        :to="'/' +contenu.type_contenu.toLowerCase() +'/' + contenu.id"
+        class="col text-decoration-none"
+        >
+        <div class="card mb-4 contenu-card">
+          <div class="card-body">
+            <h5 class="card-title">{{ contenu.description_contenu }}</h5>
+            <p class="card-text"><small class="text-muted">{{ format(Date(contenu.date_contenu), 'dd/MM/yyyy') }}</small></p>
+            <p class="card-text">{{ contenu.type_contenu }}</p>
+          </div>
+        </div>
+      </router-link>
+      </div>
+    </div>
   </div>
-
-
-
 </template>
+
 
 <script setup>
 import { format } from 'date-fns';
@@ -36,10 +32,11 @@ const contenus = ref([]);
 const libelle_matiere = ref('');
 
 
+
 onMounted(() => {
   const route = useRoute()
   const id = route.params.id;
-  axios.get(`http://localhost:3001/contenus/${id}`)
+  axios.get(`http://localhost:3001/matieres/${id}`)
     .then(response => {
       contenus.value = response.data;
       libelle_matiere.value = response.data[0].libelle_matiere;
--- model/ModeleMatiere.mjs
+++ model/ModeleMatiere.mjs
@@ -25,6 +25,32 @@ class ModeleMatiere {
         });
     }
 
+
+    recupererContenusParMatiere(id,callback) {
+      const query = 'SELECT * from Contenu c, Matiere m where c.id_matiere = m.id_matiere and m.id_matiere = ?';
+      this.connection.query(query,[id], (error, results, fields) => {
+        if (error) {
+          callback(error, null);
+          return;
+        }
+        if (results.length === 0) {
+          callback(null, null); // Aucun contenu dans cette matière
+          return;
+        }
+        const contenus_matière = results.map(row => ({
+          id: row.id_contenu,
+          description_contenu: row.description_contenu,
+          date_contenu: row.date_contenu,
+          id_matiere: row.id_matiere,
+          libelle_matiere: row.libelle_matiere,
+          type_contenu: row.type_contenu
+        }));
+  
+  
+        callback(null, contenus_matière);
+      });
+    }
+
     recupererLibelleMatiereParId(id, callback) {
       const query = 'SELECT libelle_matiere FROM Matiere WHERE id_matiere = ?';
       this.connection.query(query, [id], (error, results, fields) => {
