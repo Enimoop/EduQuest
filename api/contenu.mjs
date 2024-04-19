982ea644244f3c9faf0116fd1d41bab5435347bf
Enimoop <marine.forcioli@laposte.net>
2024-04-19 14:00:50 +0200

    Add component ListeContenu

--- api/contenu.mjs
+++ api/contenu.mjs
@@ -14,20 +14,50 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/cours', (req, res) => {
+  modeleContenu.recupererTousLesCours((error, cours) => {
+
+    if (error) {
+      res.status(500).json({ message: 'Erreur lors de la récupération des cours' });
+      return;
+    }
+    res.json(cours);
+  });
+
+});
+
+router.get('/cours/:id', (req, res) => {
+  const id = req.params.id;
+  modeleContenu.recupererCoursParId(id, (error, cours) => {
+    if (error) {
+      res.status(500).json({ message: 'Erreur lors de la récupération du cours' });
+      return;
+    }
+    if (!cours) {
+      res.status(404).json({ message: 'Cours non trouvé' });
+      return;
+    }
+    res.json(cours);
+  });
+});
 
 router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    modeleContenu.recupererContenusParMatiere(id, (error, contenus_matiere) => {
-      if (error) {
-        res.status(500).json({ message: 'Erreur lors de la récupération des contenus' });
-        return;
-      }
-      if (!contenus_matiere) {
-        res.status(404).json({ message: 'contenu non trouvé' });
-        return;
-      }
-      res.json(contenus_matiere);
-    });
+  const id = req.params.id;
+  modeleContenu.recupererContenuParId(id, (error, contenu) => {
+    if (error) {
+      res.status(500).json({ message: 'Erreur lors de la récupération du contenu' });
+      return;
+    }
+    if (!contenu) {
+      res.status(404).json({ message: 'Contenu non trouvé' });
+      return;
+    }
+    res.json(contenu);
   });
+});
+
+
+
+
 
 export default router;
