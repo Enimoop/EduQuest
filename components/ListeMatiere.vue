073277e79ff1130b71bc3a349385ee622f40c5e7
Enimoop <marine.forcioli@laposte.net>
2024-04-19 14:02:46 +0200

    Add page matiere

--- components/ListeMatiere.vue
+++ components/ListeMatiere.vue
@@ -1,17 +1,22 @@
 <template>
-  <!-- <h3>Listes des matières</h3> -->
-  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
-    <div v-for="matiere in matieres" :key="matiere.id" class="col mb-4">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">{{ matiere.libelle_matiere }}</h5>         
-          <a :href="'/contenu/' +matiere.id" class="card-link">Card link</a>
+  <div class="container mt-5">
+    <h2 class="text-center mb-5">Explorez nos matières</h2>
+    <div class="row row-cols-2 row-cols-md-5 g-4">
+      <router-link
+        v-for="matiere in matieres"
+        :key="matiere.id"
+        :to="'/matiere/' + matiere.id"
+        class="col text-decoration-none"
+      >
+        <div class="card h-75 border-0 shadow-sm matiere-card">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title text-center">{{ matiere.libelle_matiere }}</h5>
+          </div>
         </div>
-      </div>
+      </router-link>
     </div>
   </div>
 </template>
-
 
 <script setup>
   import { ref, onMounted } from 'vue';
---
+++ pages/matiere.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <br>
+    <ListeMatiere :matieres="matieres" />
+    <NuxtPage />
+</div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+import ListeMatiere from '../components/ListeMatiere.vue';
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+const matieres = ref([]);
+
+</script>
