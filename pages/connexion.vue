3e676a1efa9ac93cfc3b9680a88000c6dc2f2494
Enimoop <marine.forcioli@laposte.net>
2024-04-23 23:38:24 +0200

    Add formConnexion component and page connexion

---
+++ pages/connexion.vue
@@ -0,0 +1,11 @@
+<template>  
+<FormConnexion/>
+</template>
+
+<script setup>
+
+import { ref, computed } from 'vue';
+  import FormConnexion from '../components/FormConnexion.vue';
+  import 'bootstrap/dist/css/bootstrap.css';
+  import { useRouter } from 'vue-router';
+</script>
