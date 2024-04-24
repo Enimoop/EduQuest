d7f66ce37047bf29119fc938babb0bc3a6acacf1
Enimoop <marine.forcioli@laposte.net>
2024-04-24 02:06:20 +0200

    use of createSession in FormConnexion

--- components/FormConnexion.vue
+++ components/FormConnexion.vue
@@ -2,15 +2,15 @@
     <div class="container">
         <h1>Connexion</h1>
         <form @submit.prevent="submitForm">
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" v-model="email">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" v-model="password">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" v-model="email">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" v-model="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </template>
@@ -20,6 +20,7 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import axios from 'axios';
+import { createSession } from '~/utils/session';
 
 
 
@@ -32,21 +33,23 @@ const router = useRouter();
 const submitForm = () => {
 
     axios.get(`http://localhost:3001/profils/${email.value}`)
-      .then(response => {
-        const compte = response.data;
-        if (compte && compte.mdp === password.value) {
-            console.log('Connexion réussie');
-            router.push('/');
-        } else {
-            alert('Email ou mot de passe incorrect');
-        }
-      })
-      .catch(error => {
-        if (error.response.status === 404) {
-            alert('Aucun compte trouvé pour cet email');
-        } else {
-            console.error('Erreur lors de la récupération du compte:', error);
-        }
-      });
+        .then(response => {
+            const compte = response.data;
+            if (compte && compte.mdp === password.value) {
+                console.log('Connexion réussie');
+
+                createSession(compte);
+                router.push('/');
+            } else {
+                alert('Email ou mot de passe incorrect');
+            }
+        })
+        .catch(error => {
+            if (error.response.status === 404) {
+                alert('Aucun compte trouvé pour cet email');
+            } else {
+                console.error('Erreur lors de la récupération du compte:', error);
+            }
+        });
 }
 </script>
