e16b3401dd835930b59d5fd7383eb8c7b8a02955
Enimoop <marine.forcioli@laposte.net>
2024-04-28 13:00:42 +0200

    Update app.vue remove header and footer

--- app.vue
+++ app.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
-const router = useRouter()
 
-
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-
- 
 </script>
 
 
 
 <template>
-  <div class="main-container">
-    <Header />
     <NuxtPage />
-    <Footer />
-  </div>
 </template>
 
 
