c5232050cbfcc0cd3f503b8593819c73eeb441e7
Enimoop <marine.forcioli@laposte.net>
2024-04-30 22:57:03 +0200

    Add insert Quiz

---
+++ pages/add.vue
@@ -0,0 +1,13 @@
+<template>
+    <NuxtLayout layout="default">
+        <div>
+            <br>
+            <AjouterQuiz />
+        </div>
+    </NuxtLayout>
+</template>
+
+<script setup>
+import AjouterQuiz from '../components/AjouterQuiz.vue';
+import 'bootstrap/dist/css/bootstrap.css'
+</script>
