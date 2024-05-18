de1fe3de771c8c12120368abf06db21f79baddcd
amandine-jolibois <joliboisamandine9@gmail.com>
2024-05-18 23:55:05 +0200

    Ajout de l'import vers le composant NotesParMatiere

--- api/profil.mjs
+++ api/profil.mjs
@@ -31,6 +31,22 @@ router.get('/:mail', (req, res) => {
   });
 });
 
+
+router.get('/id/:id', (req, res) => {
+  const id = req.params.id;
+  modeleProfil.recupererUnCompteId(id, (error, profil) => {
+    if (error) {
+      res.status(500).json({ message: 'Erreur lors de la récupération du compte' });
+      return;
+    }
+    if (!profil) {
+      res.status(404).json({ message: 'compte non trouvé' });
+      return;
+    }
+    res.json(profil);
+  });
+});
+
 router.get('/type/:id', (req, res) => {
   const id = req.params.id;
   modeleProfil.checkerTypeUser(id, (error, profil) => {
@@ -45,5 +61,26 @@ router.get('/type/:id', (req, res) => {
     res.json(profil);
   });
 });
+
+router.get('/notes/:id', (req, res) => {
+  console.log('GET /notes/:id called'); // Vérifiez que la route est atteinte
+  const id = req.params.id;
+  modeleProfil.recupererNoteUnCompte(id, (error, notes) => {
+    if (error) {
+      console.error('Error retrieving notes:', error);
+      res.status(500).json({ message: 'Erreur lors de la récupération des notes' });
+      return;
+    }
+    if (!notes || notes.length === 0) {
+      console.log('No notes found for user:', id);
+      res.status(404).json({ message: 'notes non trouvées' });
+      return;
+    }
+    console.log('Notes retrieved:', notes);
+    res.json(notes);
+  });
+});
+
+
 
 export default router;
