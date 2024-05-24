486818f611b14555dcb8d219be5ebc54c9b758b7
Enimoop <marine.forcioli@laposte.net>
2024-05-24 23:31:28 +0200

    Block add page for student

--- pages/add.vue
+++ pages/add.vue
@@ -13,4 +13,20 @@
 import AjouterQuiz from '../components/AjouterQuiz.vue';
 import AjouterCours from '../components/AjouterCours.vue';
 import 'bootstrap/dist/css/bootstrap.css'
+import { getSubFromToken, returnUserType } from "../utils/session.mjs";
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const headers = useRequestHeaders(["cookie"]);
+
+const { data: token } = await useFetch("/api/token", { headers });
+
+const id = getSubFromToken(token.value);
+
+const type = await returnUserType(id);
+
+if (type !== "Prof" || type !== "Admin") {
+    router.push("/");
+}
 </script>
