9b93aef859418ba94422185635b42b9e49a7451a
Enimoop <marine.forcioli@laposte.net>
2024-06-04 12:32:06 +0200

    Fix titre contenu Notes Par Matiere

--- model/ModeleEleve.mjs
+++ model/ModeleEleve.mjs
@@ -189,7 +189,7 @@ class ModeleEleve {
 
   recupererNotesParContenu(id, callback) {
     const query = `
-        SELECT n.note, n.id_contenu, n.id_u, n.date_note, c.description_contenu
+        SELECT n.note, n.id_contenu, n.id_u, n.date_note, c.titre_contenu, c.description_contenu
         FROM Noter n
         JOIN Contenu c ON n.id_contenu = c.id_contenu
         WHERE n.id_u = ?
@@ -210,6 +210,7 @@ class ModeleEleve {
             id_contenu: row.id_contenu,
             id_u: row.id_u,
             date_note: row.date_note,
+            titre_contenu: row.titre_contenu,
             description_contenu: row.description_contenu
         }));
 
